fc384b563dc130ef84f9d21c599bbbe4d1968eb4
mhartington <mikehartington@gmail.com>
2017-05-04 11:30:51 -0400

    add search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
+import { SpotifyProvider } from '../providers/spotify/spotify';
 
 @NgModule({
   declarations: [MyApp],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -17,7 +20,8 @@ import { MyApp } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    SpotifyProvider
   ]
 })
 export class AppModule {}
